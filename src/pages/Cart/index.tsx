3c1750cfb21c546dd4db54943eef5272165d2b20
brunoNasc <emanuelbrunobn@rede.ulbra.br>
2025-04-16 13:53:49 -0300

    feat : revisao praticda de map,filter,include, reduce

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from 'react';
-import { Trash } from '@phosphor-icons/react';
+import { ArrowCounterClockwise, Trash } from '@phosphor-icons/react';
 
 import { QuantityInput } from '../../components/Form/QuantityInput';
 import {
@@ -12,6 +12,7 @@ import {
   InfoContainer,
 } from './styles';
 import { Tags } from '../../components/CoffeeCard/styles';
+import { current } from 'immer';
 
 export interface Item {
   id: string;
@@ -34,6 +35,125 @@ interface CoffeeInCart {
 }
 
 const DELIVERY_PRICE = 3.75;
+
+//////
+
+const carto = [
+  {
+    id: 1,
+    name: 'Café Expresso',
+    price: 9.5, // total 72     19
+    quantity: 2,
+    category: 'bebida',
+    paymentMethod: 'cartão',
+  },
+  {
+    id: 2,
+    name: 'Pão de Queijo',
+    price: 5.0, //
+    quantity: 3,
+    category: 'lanche',
+    paymentMethod: 'dinheiro',
+  },
+  {
+    id: 3,
+    name: 'Cappuccino',
+    price: 12.0, //12
+    quantity: 1,
+    category: 'bebida',
+    paymentMethod: 'pix',
+  },
+  {
+    id: 4,
+    name: 'Croissant',
+    price: 8.0, //16 19     35
+    quantity: 3,
+    category: 'lanche',
+    paymentMethod: 'cartão',
+  },
+  {
+    id: 5,
+    name: 'Latte',
+    price: 10.0, // 10
+    quantity: 2,
+    category: 'bebida',
+    paymentMethod: 'dinheiro',
+  },
+];
+
+//1 Filtrar todos os itens da categoria bebida
+
+const newList = carto
+  .filter((item) => item.category === 'bebida')
+  .map((item) => {
+    return item.name;
+  });
+
+//console.log(newList);
+
+//2 Somar o valor total dos itens do carrinho.
+
+const soma = carto.reduce((acc, item) => acc + item.price * item.quantity, 0); //meu preetier nao reage bem ao formatar
+
+//console.log(soma); //saida : 72
+
+//3 Separar os produto pelo metodo de pagamento e armazenar em 3 arrays : cartao, dinheiro e pix.
+
+const [array1, array2, array3] = [
+  carto.filter((item) => item.paymentMethod === 'cartão'),
+  carto.filter((item) => item.paymentMethod === 'dinheiro'),
+  carto.filter((item) => item.paymentMethod === 'pix'),
+];
+
+//console.log(array1);
+//console.log(array2);
+//console.log(array3);
+
+//4 Criar uma funcao que remova um item do carrinho pelo id.
+//number no type eh tipo primitivo Number eh interface, funfa, mas nao use
+function removeById(id: number) {
+  const newCartList = carto.filter((item) => item.id !== id);
+  // console.log(newCartList);
+}
+removeById(5);
+//5 Criar uma funcao que incremente a quantidade de um item pelo id
+
+function incrementById(id: number, many: number) {
+  const newList = carto.map((item) =>
+    item.id === id ? { ...item, quantity: item.quantity + many } : item
+  );
+
+  //console.log(newList); //saida esperada  late : 3
+}
+
+incrementById(5, 9); //saida esperada  late : 10
+
+//6 Criar uma função que retorne a soma total dos itens pagos com cartão
+
+function retornaTotal() {
+  const nova = carto
+    .filter((item) => item.paymentMethod === 'cartão')
+    .reduce((acc, item) => acc + item.price * item.quantity, 0);
+  //console.log(nova); //esperado 17.5
+  return nova;
+}
+retornaTotal();
+
+//7 Criar uma lista sem repeticao com os nomes das categorias existentes.
+
+const categoriesList: string[] = [];
+carto.map((item) => {
+  if (!categoriesList.includes(item.category)) {
+    categoriesList.push(item.category);
+  }
+});
+//console.log(categoriesList);
+
+//8 Mostrar todos os produtos cujo total(price*quantity) seja maior que 20
+
+console.log(carto.filter((item) => item.price * item.quantity >= 20));
+
+////////
 
 export function Cart() {
   const [coffeesInCart, setCoffeesInCart] = useState<CoffeeInCart[]>([
@@ -96,13 +216,24 @@ export function Cart() {
   }
 
   function handleItemDecrement(itemId: string) {
-    // coloque seu código aqui
+    const newListCoffees2 = coffeesInCart.map((coffee) =>
+      coffee.id === itemId && coffee.quantity > 1
+        ? {
+            ...coffee,
+            quantity: coffee.quantity - 1,
+            subTotal: coffee.price * coffee.quantity,
+          }
+        : coffee
+    );
+    setCoffeesInCart(newListCoffees2);
   }
 
   function handleItemRemove(itemId: string) {
-    // coloque seu código aqui
+    const newListCoffees3 = coffeesInCart.filter(
+      (coffee) => coffee.id !== itemId
+    );
+    setCoffeesInCart(newListCoffees3);
   }
-
   return (
     <Container>
       <InfoContainer>
